c538ebe6d15a41da9d5394bcde28f80f92e4c85a
ilan <ilan@fonz.net>
2019-08-01 00:05:00 +0000

    [django 1.11] request.POST now returns multidict error when a key is missing switch to request.post.get

--- scalereg/reg6/staff/views.py
+++ scalereg/reg6/staff/views.py
@@ -33,9 +33,9 @@ def CheckIn(request):
       })
 
   attendees = []
-  if request.POST['verisign']:
+  if request.POST.get('verisign'):
     orders = models.Order.objects.filter(payment_type='verisign',
-        pnref=request.POST['verisign'])
+        pnref=request.POST.get('verisign'))
     if orders.count() == 1:
       attendees = models.Attendee.objects.filter(valid=True, order=orders[0])
 
@@ -57,18 +57,18 @@ def CheckIn(request):
     if success:
       attendees = [attendee]
 
-  if not attendees and request.POST['last_name']:
+  if not attendees and request.POST.get('last_name'):
     attendees = models.Attendee.objects.filter(valid=True,
-        last_name__icontains=request.POST['last_name'])
+        last_name__icontains=request.POST.get('last_name'))
 
-  if not attendees and request.POST['zip']:
+  if not attendees and request.POST.get('zip'):
     attendees = models.Attendee.objects.filter(valid=True,
-        zip=request.POST['zip'])
+        zip=request.POST.get('zip'))
 
   attendees = [att for att in attendees]
-  if request.POST['last_name']:
+  if request.POST.get('last_name'):
     bad_attendees = models.Attendee.objects.filter(valid=False,
-        last_name__icontains=request.POST['last_name'])
+        last_name__icontains=request.POST.get('last_name'))
     attendees += bad_attendees
 
   for att in attendees:
@@ -83,10 +83,10 @@ def CheckIn(request):
   return render_to_response('reg6/staff/checkin.html',
     {'title': 'Attendee Check In',
      'attendees': attendees,
-     'express': request.POST['express'],
-     'last_name': request.POST['last_name'],
-     'verisign': request.POST['verisign'],
-     'zip': request.POST['zip'],
+     'express': request.POST.get('express'),
+     'last_name': request.POST.get('last_name'),
+     'verisign': request.POST.get('verisign'),
+     'zip': request.POST.get('zip'),
      'search': 1,
     })
 
@@ -104,13 +104,16 @@ def FinishCheckIn(request):
     return handler500(request, msg='No ID')
 
   try:
-    attendee = models.Attendee.objects.get(id=request.POST['id'])
+    attendee = models.Attendee.objects.get(id=request.POST.get('id'))
   except models.Attendee.DoesNotExist:
     return handler500(request, msg='We could not find your registration')
 
   try:
     attendee.checked_in = True
     attendee.save()
+    kiosk_agents = models.KioskAgent.objects.filter(attendee=attendee)
+    for agent in kiosk_agents:
+      agent.delete()
   except:
     return handler500(request, msg='We encountered a problem with your checkin')
 
@@ -134,7 +137,7 @@ def CashPayment(request):
       })
 
   for var in ['FIRST', 'LAST', 'EMAIL', 'ZIP', 'TICKET']:
-    if var not in request.POST or not request.POST[var]:
+    if var not in request.POST or not request.POST.get(var):
       return render_to_response('reg6/staff/cash.html',
         {'title': 'Cash Payment',
          'tickets': tickets,
@@ -142,7 +145,7 @@ def CashPayment(request):
         })
 
   try:
-    ticket = models.Ticket.objects.get(name=request.POST['TICKET'])
+    ticket = models.Ticket.objects.get(name=request.POST.get('TICKET'))
   except:
     return render_to_response('reg6/staff/cash.html',
       {'title': 'Cash Payment',
@@ -156,20 +159,24 @@ def CashPayment(request):
   order.order_num = GenerateOrderID(bad_order_nums)
   assert order.order_num
   order.valid = True
-  order.name = '%s %s' % (request.POST['FIRST'], request.POST['LAST'])
+  order.name = '%s %s' % (request.POST.get('FIRST'), request.POST.get('LAST'))
   order.address = 'Cash'
   order.city = 'Cash'
   order.state = 'Cash'
-  order.zip = request.POST['ZIP']
-  order.email = request.POST['EMAIL']
-  order.payment_type = 'cash'
+  order.zip = request.POST.get('ZIP')
+  order.email = request.POST.get('EMAIL')
+  # FIXME add a field in the Model or something.
+  if ticket.name in ('GUEST', 'KID', 'STAFF'):
+    order.payment_type = 'invitee'
+  else:
+    order.payment_type = 'cash'
   order.amount = ticket.price
 
   attendee = models.Attendee()
-  attendee.first_name = request.POST['FIRST']
-  attendee.last_name = request.POST['LAST']
-  attendee.zip = request.POST['ZIP']
-  attendee.email = request.POST['EMAIL']
+  attendee.first_name = request.POST.get('FIRST')
+  attendee.last_name = request.POST.get('LAST')
+  attendee.zip = request.POST.get('ZIP')
+  attendee.email = request.POST.get('EMAIL')
   attendee.valid = True
   attendee.checked_in = True
   attendee.can_email = False
@@ -203,15 +210,15 @@ def CashPaymentRegistered(request):
   if request.method == 'GET':
     return handler500(request, msg='POST only.')
 
-  if not 'id' in request.POST or not request.POST['id']:
+  if not 'id' in request.POST or not request.POST.get('id'):
     return handler500(request, msg='missing data: no %s field' % var)
 
   try:
-    attendee = models.Attendee.objects.get(id=request.POST['id'])
+    attendee = models.Attendee.objects.get(id=request.POST.get('id'))
   except:
     return handler500(request, msg='cannot find attendee')
 
-  if not 'action' in request.POST or request.POST['action'] != 'pay':
+  if not 'action' in request.POST or request.POST.get('action') != 'pay':
     return render_to_response('reg6/staff/cash_registered.html',
       {'title': 'Cash Payment For Registered Attendee',
        'attendee': attendee,
@@ -260,14 +267,14 @@ def Email(request):
     return HttpResponseRedirect('/reg6/staff/checkin/')
 
   EMAIL_TEMPLATE = 'reg6/staff/email.html'
-  if not 'id' in request.POST or not request.POST['id']:
+  if not 'id' in request.POST or not request.POST.get('id'):
     return render_to_response(EMAIL_TEMPLATE,
       {'title': 'Attendee Email Invalid',
        'attendee': None,
       })
 
   try:
-    attendee = models.Attendee.objects.get(id=int(request.POST['id']))
+    attendee = models.Attendee.objects.get(id=int(request.POST.get('id')))
     NotifyAttendee(attendee)
   except:
     attendee = None
@@ -287,34 +294,102 @@ def Reprint(request):
   if request.method == 'GET':
     return HttpResponseRedirect('/reg6/staff/checkin/')
 
-  if request.POST['id']:
-    REPRINT_TEMPLATE = 'reg6/staff/reprint.html'
+  REPRINT_TEMPLATE = 'reg6/staff/reprint.html'
+  if not request.POST.get('id'):
+    return render_to_response(REPRINT_TEMPLATE,
+        {'title': 'Attendee Reprint Invalid',
+         'reprint': None,
+        })
+
+  try:
+    attendee = models.Attendee.objects.get(id=int(request.POST.get('id')))
     try:
-      attendee = models.Attendee.objects.get(id=int(request.POST['id']))
-      reprint = None
-      try:
-        reprint = models.Reprint.objects.get(attendee=attendee)
-      except:
-        reprint = models.Reprint()
-        reprint.attendee = attendee
-        reprint.count = 0
-      reprint.count += 1
-      reprint.save()
-      return render_to_response(REPRINT_TEMPLATE,
+      reprint = models.Reprint.objects.get(attendee=attendee)
+    except:
+      reprint = models.Reprint()
+      reprint.attendee = attendee
+      reprint.count = 0
+
+    kiosk_agents = models.KioskAgent.objects.filter(attendee=attendee)
+    for agent in kiosk_agents:
+      agent.delete()
+
+    reprint.count += 1
+    reprint.save()
+    return render_to_response(REPRINT_TEMPLATE,
         {'title': 'Attendee Reprint',
          'reprint': reprint,
         })
-    except:
-      return render_to_response(REPRINT_TEMPLATE,
+  except:
+    return render_to_response(REPRINT_TEMPLATE,
         {'title': 'Attendee Reprint Error',
          'reprint': None,
         })
-  else:
-    return render_to_response(REPRINT_TEMPLATE,
-      {'title': 'Attendee Reprint Invalid',
-       'reprint': None,
+
+
+@login_required
+def UpdateAttendee(request):
+  can_access = services_perm_checker(request.user, request.path)
+  if not can_access:
+    return HttpResponseRedirect('/accounts/profile/')
+
+  if request.method == 'GET':
+    return handler500(request, msg='POST only.')
+
+  if not 'id' in request.POST or not request.POST.get('id'):
+    return handler500(request, msg='missing data: no %s field' % var)
+
+  try:
+    attendee = models.Attendee.objects.get(id=request.POST.get('id'))
+  except:
+    return handler500(request, msg='cannot find attendee')
+
+  if not 'action' in request.POST or request.POST.get('action') != 'update':
+    return render_to_response('reg6/staff/update_attendee.html',
+      {'title': 'Update Attendee',
+       'attendee': attendee,
+       'orig_salutation': attendee.salutation,
+       'orig_first_name': attendee.first_name,
+       'orig_last_name': attendee.last_name,
+       'orig_title': attendee.title,
+       'orig_org': attendee.org,
+       'orig_email': attendee.email,
+       'orig_zip': attendee.zip,
+       'orig_phone': attendee.phone,
+       'salutations': models.SALUTATION_CHOICES,
       })
 
+  for var in ['SALUTATION', 'FIRST', 'LAST', 'TITLE', 'ORG', 'EMAIL', 'ZIP',
+              'PHONE', 'ORIG_SALUTATION', 'ORIG_FIRST', 'ORIG_LAST',
+              'ORIG_TITLE', 'ORIG_ORG', 'ORIG_EMAIL', 'ORIG_ZIP', 'ORIG_PHONE']:
+    if var not in request.POST:
+      return handler500(request, msg='missing data: no %s field' % var)
+
+  attendee.salutation = request.POST.get('SALUTATION')
+  attendee.first_name = request.POST.get('FIRST')
+  attendee.last_name = request.POST.get('LAST')
+  attendee.title = request.POST.get('TITLE')
+  attendee.org = request.POST.get('ORG')
+  attendee.email = request.POST.get('EMAIL')
+  attendee.zip = request.POST.get('ZIP')
+  attendee.phone = request.POST.get('PHONE')
+  try:
+    attendee.full_clean()
+  except:
+    return render_to_response('reg6/staff/update_attendee.html',
+      {'title': 'Update Attendee',
+       'attendee': attendee,
+       'orig_salutation': request.POST.get('ORIG_SALUTATION'),
+       'orig_first_name': request.POST.get('ORIG_FIRST'),
+       'orig_last_name': request.POST.get('ORIG_LAST'),
+       'orig_title': request.POST.get('ORIG_TITLE'),
+       'orig_org': request.POST.get('ORIG_ORG'),
+       'orig_email': request.POST.get('ORIG_EMAIL'),
+       'orig_zip': request.POST.get('ORIG_ZIP'),
+       'orig_phone': request.POST.get('ORIG_PHONE'),
+       'salutations': models.SALUTATION_CHOICES,
+       'error': True,
+      })
 
 @login_required
 def UpdateAttendee(request):
@@ -388,14 +463,14 @@ def UpdateAttendee(request):
   return render_to_response('reg6/staff/update_attendee.html',
     {'title': 'Cash Payment For Registered Attendee',
      'attendee': attendee,
-     'orig_salutation': request.POST['ORIG_SALUTATION'],
-     'orig_first_name': request.POST['ORIG_FIRST'],
-     'orig_last_name': request.POST['ORIG_LAST'],
-     'orig_title': request.POST['ORIG_TITLE'],
-     'orig_org': request.POST['ORIG_ORG'],
-     'orig_email': request.POST['ORIG_EMAIL'],
-     'orig_zip': request.POST['ORIG_ZIP'],
-     'orig_phone': request.POST['ORIG_PHONE'],
+     'orig_salutation': request.POST.get('ORIG_SALUTATION'),
+     'orig_first_name': request.POST.get('ORIG_FIRST'),
+     'orig_last_name': request.POST.get('ORIG_LAST'),
+     'orig_title': request.POST.get('ORIG_TITLE'),
+     'orig_org': request.POST.get('ORIG_ORG'),
+     'orig_email': request.POST.get('ORIG_EMAIL'),
+     'orig_zip': request.POST.get('ORIG_ZIP'),
+     'orig_phone': request.POST.get('ORIG_PHONE'),
      'salutations': models.SALUTATION_CHOICES,
      'success': True,
     })
