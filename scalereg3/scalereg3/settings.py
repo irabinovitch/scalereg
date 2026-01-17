a8cea9a2f16886fe759e239d5fef617e68f0816a
Ilan Rabinovitch <ilan@fonz.net>
2026-01-17 23:59:26 +0000

    feat: add middleware to log admin request host headers for debugging

--- scalereg3/scalereg3/settings.py
+++ scalereg3/scalereg3/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'common.middleware.AdminHostLoggingMiddleware',
 ]
 
 ROOT_URLCONF = 'scalereg3.urls'
